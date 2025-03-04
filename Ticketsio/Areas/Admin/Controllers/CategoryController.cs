2e490b34ca1f910f1efda118a4d8328c7aec9a1b
Abdullah Mahmoud <abdullahmoud123@gmail.com>
2025-03-05 00:54:27 +0200

    Adding admin area with Adding CategoryController, abit of the CinemaController with CRUD.

---
+++ Ticketsio/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ticketsio.Models;
+using Ticketsio.Repository.IRepositories;
+
+namespace Ticketsio.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryRepository categoryRepository;
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+        public IActionResult Index()
+        {
+           var categories = categoryRepository.Get();
+            return View(categories.ToList());
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if(ModelState.IsValid)
+            {
+                categoryRepository.Create(category);
+                categoryRepository.Commit();
+                CookieOptions cookieOptions = new CookieOptions()
+                {
+                    Expires = DateTime.Now.AddSeconds(10),
+                    Secure = true
+                };
+                Response.Cookies.Append("notification", "Created Category Successfuly", cookieOptions);
+                return RedirectToAction("Index");
+            }
+           return View(category);
+        }
+        public IActionResult Delete(int id)
+        {
+            var category = categoryRepository.GetOne(e => e.Id == id);
+            if (category != null)
+            {
+                categoryRepository.Delete(category);
+                categoryRepository.Commit();
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+          var category =  categoryRepository.GetOne(e => e.Id == id);
+        return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                categoryRepository.Edit(category);
+                categoryRepository.Commit();
+                CookieOptions cookieOptions = new CookieOptions()
+                {
+                    Expires = DateTime.Now.AddSeconds(10),
+                    Secure = true
+                };
+                Response.Cookies.Append("notification", "Edited Category Successfuly", cookieOptions);
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+    }
+}
---
+++ Ticketsio/Areas/Admin/Controllers/CinemaController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ticketsio.Models;
+using Ticketsio.Repository.IRepositories;
+
+namespace Ticketsio.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CinemaController : Controller
+    {
+        private readonly ICinemaRepository cinemaRepository;
+        public CinemaController(ICinemaRepository cinemaRepository)
+        {
+            this.cinemaRepository = cinemaRepository;
+        }
+        public IActionResult Index()
+        {
+            var cinemas = cinemaRepository.Get();
+            return View(cinemas);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Cinema cinema)
+        {
+            if (ModelState.IsValid) { 
+            cinemaRepository.Create(cinema);
+                cinemaRepository.Commit();
+                CookieOptions cookieOptions = new CookieOptions()
+                {
+                    Expires = DateTime.Now.AddSeconds(10),
+                    Secure = true
+                };
+                Response.Cookies.Append("notification", "Created Cinema Successfuly", cookieOptions);
+                return RedirectToAction("Index");
+            }
+            return View(cinema);
+        }
+        public IActionResult Delete(int id)
+        {
+            var cinema = cinemaRepository.GetOne(e  => e.Id == id);
+            if(cinema != null)
+            {
+            cinemaRepository.Delete(cinema);
+                cinemaRepository.Commit();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var cinema = cinemaRepository.GetOne(e =>e.Id == id);
+            return View(cinema);
+        }
+        [HttpPost]
+        public IActionResult Edit(Cinema cinema)
+        {
+            if (ModelState.IsValid) {
+                cinemaRepository.Edit(cinema);
+                cinemaRepository.Commit();
+                return RedirectToAction("Index");
+            }
+            return View(cinema);
+        }
+    }
+}
