37c1c6a890fd071fd3fb2c2ccb70df8281df3844
Abdullah Mahmoud <abdullahmoud123@gmail.com>
2025-03-05 02:47:39 +0200

    adding MovieController + Index

---
+++ Ticketsio/Areas/Admin/Controllers/MovieController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ticketsio.Repository.IRepositories;
+
+namespace Ticketsio.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MovieController : Controller
+    {
+        private readonly IMovieRepository movieRepository;
+        public MovieController(IMovieRepository movieRepository)
+        {
+            this.movieRepository = movieRepository;
+        }
+        public IActionResult Index()
+        {
+            var movies = movieRepository.Get();
+            return View(movies);
+        }
+    }
+}
